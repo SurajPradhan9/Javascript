2d49c8dc9425b061705e32a1ca58b680b9f38b6f
Suraj Pradhan <135151764+SurajPradhan9@users.noreply.github.com>
2024-05-04 04:34:10 +0000

    operations discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,49 @@ let stringNumber = String(someNumber)
 // console.log(typeof stringNumber);
 
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " suraj"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2); //122 (first value is string so it is treated as string)
+// console.log(1 + 2 + "2");//32
+
+// console.log( (3 + 4) * 5 % 3);//2 (bracket one is executed first)
+
+// console.log(true)//true
+// console.log(+true)//1
+// console.log(+"");//0
+
+//not best practice
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// console.log(++gameCounter);
+// console.log(gameCounter++);
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
 
 
 
