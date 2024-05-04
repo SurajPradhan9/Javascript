fc763a9da0f26a65fcdfce5face1f4e32c3c17be
Suraj Pradhan <135151764+SurajPradhan9@users.noreply.github.com>
2024-05-04 02:26:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser ,so use in console after inspecting any page we have an alert for nodejs too we will see furthur
+
+console.log(3
+    +
+    3)//code readability should be high
+
+console.log("suraj")
+//the above 2 consoles will give the output because javascript ignores spaces but it is not the right way. our code should be readable and maintainable
+
+//for documentation refer 2 websites mdn and https://tc39.es/ecma262/
+
+
+let name = "suraj"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//Primitive datatypes
+// number => range 2 to power 53
+// bigint=>for big vlaues
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique (to justify uniqueness we use symbol datatype)
+
+// object //we will see later
+
+console.log(typeof "suraj")//string
+console.log(typeof age)//number
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+
+
+
